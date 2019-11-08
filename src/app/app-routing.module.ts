3f4573f2331b352b9a6ed8092beb56bd5c461e77
Leandro Silva <leandronstrab@gmail.com>
2019-11-08 16:32:31 -0300

    Aplicando css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {  ModuleWithProviders } from '@angular/core';
+import { Routes,RouterModule } from "@angular/router";
+
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+
+const APP_ROUTES:Routes=[
+  { path:'', component: HomeComponent},
+  { path:'login', component: LoginComponent},
+  { path: 'cursos',component:CursosComponent}
+];
 
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const routing:ModuleWithProviders=RouterModule.forRoot(APP_ROUTES);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {routing} from './app-routing.module' 
 import { AppComponent } from './app.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursosComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
